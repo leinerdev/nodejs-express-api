e5004ca42391b9bae4441de802729868a40984a0
leinerdev <leinerbarrios99.ljbm@gmail.com>
2024-09-22 11:33:14 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.json({ message: '¡Hola desde mi primera API!' });
+});
+
+app.listen(port, () => {
+  console.log(`API corriendo en http://localhost:${port}`);
+});
